567e89d3dc8d0366a0fe5ccd3c5616b194f9bde3
Antonino D'Anna <anda982008@gmail.com>
2024-07-30 12:52:28 +0200

    add automatic flush everytime that something is written in a log

--- src/Logging_write.jl
+++ src/Logging_write.jl
@@ -1,9 +1,11 @@
 function Logging_write(fb::IOStream,x...)
   println(fb, x...)
+  flush(fb)
 end
 
 function Logging_write(fb::IOStream,x::Vector)
   for i in x
     println(fb,i)
   end
+  flush(fb)
 end
